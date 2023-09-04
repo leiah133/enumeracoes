0352c0882c5b828fe35d0be5a060becd046e6285
Léiah Oliver <leiaanjos87@gmail.com>
2023-09-03 22:25:51 -0300

    criação das entidades, e as configurações iniciais do Order e OrderStatus

---
+++ src/entities/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package entities.enums;
+
+public enum OrderStatus {
+    //aguardando pagamento
+    PENDING_PAYAMENT,
+    //processando
+    PROCESSING,
+    //enviado
+    SHIPPED,
+    //entregue
+    DEVILERED;
+}
